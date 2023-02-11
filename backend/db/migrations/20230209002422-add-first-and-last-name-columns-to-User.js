a7daa031c43eecf07f27f46c9eb945c6d2c4f2bb
bergmazz <ebergmann@uarts.edu>
2023-02-11 13:47:38 -0500

    added options object to addcolumn migration up and down

--- backend/db/migrations/20230209002422-add-first-and-last-name-columns-to-User.js
+++ backend/db/migrations/20230209002422-add-first-and-last-name-columns-to-User.js
@@ -8,12 +8,12 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-            await queryInterface.addColumn("Users", "firstName", { type: Sequelize.STRING });
-            await queryInterface.addColumn("Users", "lastName", { type: Sequelize.STRING});
+            await queryInterface.addColumn( options, "firstName", { type: Sequelize.STRING });
+            await queryInterface.addColumn( options, "lastName", { type: Sequelize.STRING});
   },
 
   async down (queryInterface, Sequelize) {
-   await queryInterface.removeColumn("Users", "firstName");
-   await queryInterface.removeColumn("Users", "lastName");
+   await queryInterface.removeColumn(options, "firstName");
+   await queryInterface.removeColumn(options, "lastName");
   }
 };
