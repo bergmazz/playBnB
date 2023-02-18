b91d9b3ec3e963ecb991cc067d33c1bc8333b3cb
bergmazz <ebergmann@uarts.edu>
2023-02-18 14:59:22 -0500

    default scope working for spots, returning avg rating and preview for get all spots as expected

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -34,85 +34,129 @@ module.exports = (sequelize, DataTypes) => {
 
     }
   }
-  Spot.init({
-        ownerId: {
-              type: DataTypes.INTEGER,
-              allowNull: false,
-            validate: {
-            notEmpty: true
-            }
-
+  Spot.init(
+    {
+      ownerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        address: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                   notEmpty: true
-             }
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        city: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                   notEmpty: true
-             }
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        state: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                   notEmpty: true
-             }
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        country: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                   notEmpty: true
-             }
+      },
+      country: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        lat: {
-              type: DataTypes.FLOAT,
-              validate: {
-                  min: -90,
-                  max: 90
-            }
+      },
+      lat: {
+        type: DataTypes.FLOAT,
+        validate: {
+          min: -90,
+          max: 90,
         },
-        lng: {
-              type: DataTypes.FLOAT,
-                  validate: {
-                  min: -180,
-                  max: 180
-            }
+      },
+      lng: {
+        type: DataTypes.FLOAT,
+        validate: {
+          min: -180,
+          max: 180,
         },
-        name: {
-            type: DataTypes.STRING,
-            validate: {
-                  len: [1, 50]
-            }
+      },
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [1, 50],
         },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                   notEmpty: true
-             }
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
         },
-        price: {
-            type: DataTypes.NUMERIC,
-            allowNull: false,
-            validate: {
-                   notEmpty: true
-             }
-        }
-  }, {
-    sequelize,
-      modelName: 'Spot',
-            defaultScope: {
-                  attributes: {
-                  exclude: ["createdAt", "updatedAt"],
-                  },
-            },
-  });
+      },
+      price: {
+        type: DataTypes.NUMERIC,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Spot",
+      defaultScope: {
+        include: [
+          {
+            //gotta include the models in order for fn to recognize the columns
+            association: 'Reviews',
+            required: false,
+            attributes: [],
+          },
+          {
+            association: 'SpotImages',
+            required: false,
+            where: { preview: true },
+            attributes: [],
+          },
+        ],
+        attributes: [
+          "id",
+          "ownerId",
+          "address",
+          "city",
+          "state",
+          "country",
+          "lat",
+          "lng",
+          "name",
+          "description",
+          "price",
+          "createdAt",
+          "updatedAt",
+          [
+            sequelize.fn(
+              "COALESCE", //returns the first non-null value in a list
+              sequelize.fn("AVG", sequelize.col("Reviews.stars")),
+              sequelize.literal("'no ratings just yet'")
+            ), //If no reviews for a spot,  return message as a default value
+            "avgRating",
+          ],
+          [
+            sequelize.fn(
+              "COALESCE",
+              sequelize.col("SpotImages.url"),
+              sequelize.literal("'image preview unavailable'")
+            ),
+            "previewImage",
+          ],
+        ],
+      },
+    }
+  );
   return Spot;
 };
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,45 +9,11 @@ const { Spot, Review, SpotImage, User, sequelize } = require("../../db/models");
 const router = express.Router();
 
 
-router.get("/", async (req, res) => {
-
-      const Spots = await Spot.findAll({
-        attributes: {
-          include: [
-            [
-              sequelize.fn(
-                "COALESCE", //returns the first non-null value in a list
-                sequelize.fn("AVG", sequelize.col("Reviews.stars")),
-                sequelize.literal("'no ratings just yet'")
-              ), //If no reviews for a spot,  return message as a default value
-              "avgRating",
-            ],
-            [
-              sequelize.fn(
-                "COALESCE",
-                sequelize.col("SpotImages.url"),
-                sequelize.literal("'image preview unavailable'")
-              ),
-              "previewImage",
-            ],
-          ],
-        },
-        include: [
-          { //gotta include the models in order for fn to recognize the columns
-            model: Review,
-            required: false,
-            attributes: [],
-          },
-          {
-            model: SpotImage,
-            required: false,
-            where: { preview: true },
-            attributes: [],
-          },
-        ],
+router.get( "/", async ( req, res ) => {
+      const Spots = await Spot.findAll({ //using default scope
         group: ["Spot.id"], //one row per spot in the result set
-        //only get 1 result without this group specification- bc of coalesce?
-      });
+        // we only get 1 result without this group specification- bc of coalesce?
+      } );
   res.json({ Spots });
 });
 
