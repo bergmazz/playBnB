69454398f5b009faa044546b15589ca1b5f94dc1
bergmazz <ebergmann@uarts.edu>
2023-03-22 20:31:44 -0400

    logout and navigation completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -49,11 +49,20 @@ export const signup = (user) => async (dispatch) => {
       email,
       password,
     }),
-  });
+  } );
   const data = await response.json();
   dispatch(setUser(data.user));
   return response;
 };
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 
 const initialState = { user: null };
 
