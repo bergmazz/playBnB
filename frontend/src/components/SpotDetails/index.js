138a3ad0a6fad9b8557da83795f0980b6a596007
bergmazz <ebergmann@uarts.edu>
2023-04-06 12:28:28 -0400

    we're getting somewhere! elements above reviews looking good

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -30,24 +30,34 @@ const SpotDetails = () => {
                                     ) ) }
                         </div >
 
-             <div>
-                        <div className="owner">
-                                    <p>Hosted by { spot.Owner.firstName } { spot.Owner.lastName }</p>
-
-                                    <div className="booking">
+             <div className="more-info">
+                               <div>
+                              <p className="owner">Hosted by { spot.Owner.firstName } { spot.Owner.lastName }</p>
+                                    <p className="spotDescription">{ spot.description }</p>
+                              </div>
+                                    <div className="book-container">
                                                 <div className="book-price">
                                                       <p>${ spot.price } night</p>
                                                 </div>
                                                 <div className='book-star'>
-                                                      <p><i className="fa-solid fa-star"></i>{ spot.avgStarRating }</p>
+                                                      <p><i className="fa-solid fa-star"></i>{ spot.avgRating }</p>
                                                 </div>
                                                 <div className="book-review-count" >
-                                                      <p>{ spot.numReviews ? spot.numReviews : 'New' }</p>
+                                                      <p>{ spot.numReviews } reviews</p>
                                                 </div>
-                                          <button className="RSVP-btn">Reserve</button>
-                                     </div>
+                                          <button className="book-btn">Reserve</button>
+
                               </div>
-                   </div>
+                        </div>
+
+                         <div className='review-star'>
+                              <p><i className="fa-solid fa-star"></i>{ spot.avgRating }</p>
+                        </div>
+                              <div className="review-count" >
+                                    <p>{ spot.numReviews } reviews</p>
+                              </div>
+
+
              </div>
 
                   )
